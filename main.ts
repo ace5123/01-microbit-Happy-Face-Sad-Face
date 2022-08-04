856480ec8209e831f946060ed82947c9c57ea860
ace5123 <110611727+ace5123@users.noreply.github.com>
2022-08-04 18:05:00 +0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showIcon(IconNames.Sad)
+})
